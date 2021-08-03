d130722388ccd4a931b7d77b85bd2ac6b0a204a9
KhushiRajpal <khushik.rajpal2015@gmail.com>
2021-08-03 15:51:10 +0530

    min steps program dp

---
+++ OOP_lab/Untitled-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+template <typename T>
+void selection_sort(T a[], int n)
+{
+    int i, j , min_index;
+    for ( i = 0; i < n - 1; i++)
+    {
+        min_indx = i;
+    }
+}
